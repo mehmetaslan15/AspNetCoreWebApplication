2014243ec691954cc57a10e0409e7be70e70ce90
Mehmet Aslan <aslan.memett@gmail.com>
2022-08-07 13:07:34 +0300

    7 Ağustos - Brands>Create tamamlandı

---
+++ AspNetCoreWebApplication/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,97 @@
+﻿using AspNetCoreWebApplication.Data;
+using AspNetCoreWebApplication.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public BrandsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BrandsController
+        public async Task<ActionResult> Index()
+        {
+            return View(await _context.Brands.ToListAsync());
+        }
+
+        // GET: BrandsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BrandsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Brand brand)
+        {
+            try
+            {
+                await _context.Brands.AddAsync(brand);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu");
+            }
+            return View(brand);
+        }
+
+        // GET: BrandsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
